6166c7c308c82a747705707d5716f720a4637755
Vishal Mirchandani <vmirchandani@Vishals-MacBook-Pro.local>
2017-05-17 17:21:57 +0100

    Add nav search support

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,25 +1,17 @@
 var React = require("react");
 var {Link} = require("react-router");
 
-// var Examples = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>Examples Component</h3>
-//     )
-//   }
-// });
-
 var Examples = (props) => {
     return (
       <div>
         <h1 className="text-center page-title">Examples</h1>
         <p>Here are a few example locations to try out:</p>
         <ol>
-          <li><Link to="/" location="Copenhagen">Copenhagen, DK</Link></li>
-          <li><Link to="/" location="Rio">Rio, Brasil</Link></li>
-          <li><Link to="/" location="London">London, UK</Link></li>
-          <li><Link to="/" location="Paris">Paris, France</Link></li>
-          <li><Link to="/" location="Japan">Japan, Tokyo</Link></li>
+          <li><Link to="/?location=Copenhagen">Copenhagen, DK</Link></li>
+          <li><Link to="/?location=Rio">Rio, Brasil</Link></li>
+          <li><Link to="/?location=London">London, UK</Link></li>
+          <li><Link to="/?location=Paris">Paris, France</Link></li>
+          <li><Link to="/?location=Japan">Japan, Tokyo</Link></li>
         </ol>
       </div>
     )
